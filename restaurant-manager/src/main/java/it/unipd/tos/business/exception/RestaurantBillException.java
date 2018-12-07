3acf36e43da1e19e56fd0e2ac017420d936def05
AlessandroPegoraro <spampego@gmail.com>
2018-12-07 12:50:23 +0100

    Aggiornata classe eccezione RestaurantBillException

--- restaurant-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
+++ restaurant-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
@@ -4,14 +4,12 @@
 package it.unipd.tos.business.exception;
 
 public class RestaurantBillException extends Exception {
-	private String message;
-	
-	public RestaurantBillException(String error) {
-		message=error;
-	}
-	
-	public String getMessage() {
-		return message;
-	}
+private String message;
+public RestaurantBillException(String error) {
+message=error;
+}
+public String getMessage() {
+return message;
+}
 	
 }
