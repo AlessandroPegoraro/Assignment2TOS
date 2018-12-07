e06b712a092dd39ab0f9de696cf4185a320db42e
AlessandroPegoraro <spampego@gmail.com>
2018-12-07 13:13:41 +0100

    close #2 Creazione classe MenuItem

---
+++ restaurant-manager/src/main/java/it/unipd/tos/model/MenuItem.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Pegoraro 1143562
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.model;
+
+public class MenuItem {
+private enum itemType{
+Pizza, Primo
+}
+public itemType type;
+private String name;
+private double price;
+public MenuItem(itemType tipo, String nome, double prezzo) {
+this.type=tipo;
+this.name=nome;
+if(prezzo>0) {
+this.price=prezzo;
+}else {
+this.price=0.0;
+}
+}
+public itemType getType() {
+return type;
+}
+public String getName() {
+return name;
+}
+public double getPrice() {
+return price;
+}
+}
