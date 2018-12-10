27bb6ee3cb83bd58001587d9c81eb4e79ae7719c
apegorar <apegorar@t47.torre.math.unipd.it>
2018-12-10 13:07:39 +0100

    Creazione Test per sviluppare in approccio TDD

--- restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
@@ -31,11 +31,112 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void SumOfPrice_EmptyList_Expected0()
     {
-        assertTrue( true );
+        fail("Not yet implemented");
     }
+
+    @Test
+    public void SumOfPrice_MoreThan20Elements_RestaurantBillExceptionThrown()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_OnlyMoreThan20Pizze_RestaurantBillExceptionThrown()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_OnlyMoreThan20Primi_RestaurantBillExceptionThrown()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_MoreThan20PizzeAndMoreThan20Primi_RestaurantBillExceptionThrown()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan20ElementsTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan20ElementsAndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan20ElementsAnd10OrLessPizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan20PrimiTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan20AndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_LessThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
+    {
+        fail("Not yet implemented");
+    }
+
+
+
+
+ 
+    @Test
+    public void SumOfPrice_2PizzeAnd2Primi_PriceIsLessThan100Euro()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_4PrimiAnd2Pizze_PriceIsLessThan100Euro()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_3Pizze_PriceIsLessThan100Euro()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void SumOfPrice_3Primi_PriceIsLessThan100Euro()
+    {
+        fail("Not yet implemented");
+    }
+
+    
+
+    @Test
+    public void Sum_ListOfMoreThan10AndLessThan20Elements_PriceIsLessThan100EuroAndLeastExpensiveElementCosts0()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void Sum_ListOfPizzeAndPrimi()
+    {
+        fail("Not yet implemented");
+    }
+	
 }
