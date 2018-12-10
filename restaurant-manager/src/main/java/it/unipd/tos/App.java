41ad85b42e044c6d006771e7ed065f32da229261
AlessandroPegoraro <spampego@gmail.com>
2018-12-10 22:07:32 +0100

    Eliminati Tab per checkstyle.xml

--- restaurant-manager/src/main/java/it/unipd/tos/App.java
+++ restaurant-manager/src/main/java/it/unipd/tos/App.java
@@ -6,50 +6,49 @@ import it.unipd.tos.business.RestaurantBill;
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 import java.util.List;
-
 public class App implements RestaurantBill
 {
-    @Override
-    public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException
-    {
-	if(itemsOrdered.size()>20)
-        {
-             throw new RestaurantBillException("Ordinazione Fallita");
-        }
-        int sumPizze=0;
-	double minPrice=-1.0;
-	for(int i=0;i<itemsOrdered.size();i++)
-        {
-        	if(itemsOrdered.get(i).getType()==MenuItem.itemType.Pizza)
-            	{
-                	sumPizze++;
-			if(itemsOrdered.get(i).getPrice()<minPrice || minPrice<0.0)
-                        {
-				minPrice=itemsOrdered.get(i).getPrice();
-                        }
-		}
-        }
-	double totalPrice=0.0;
-		for(int i=0;i<itemsOrdered.size();i++)
-        {
-		if(sumPizze>10 && itemsOrdered.get(i).getType()==MenuItem.itemType.Pizza)
-		{
-			if(itemsOrdered.get(i).getPrice()!=minPrice)
-			{
-				totalPrice+=itemsOrdered.get(i).getPrice();
-			}else
-			{
-				minPrice=0.0;//Elimino il prezzo di una sola pizza
-			}
-		}else
-		{
-			totalPrice+=itemsOrdered.get(i).getPrice();
-		}
-	}
-	if(totalPrice>100.0)
-	{
-		totalPrice*=0.95;
-	}
-	return totalPrice;
-    }
+@Override
+public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException
+{
+if(itemsOrdered.size()>20)
+{
+throw new RestaurantBillException("Ordinazione Fallita");
+}
+int sumPizze=0;
+double minPrice=-1.0;
+for(int i=0;i<itemsOrdered.size();i++)
+{
+if(itemsOrdered.get(i).getType()==MenuItem.itemType.Pizza)
+{
+sumPizze++;
+if(itemsOrdered.get(i).getPrice()<minPrice || minPrice<0.0)
+{
+minPrice=itemsOrdered.get(i).getPrice();
+}
+}
+}
+double totalPrice=0.0;
+for(int i=0;i<itemsOrdered.size();i++)
+{
+if(sumPizze>10 && itemsOrdered.get(i).getType()==MenuItem.itemType.Pizza)
+{
+if(itemsOrdered.get(i).getPrice()!=minPrice)
+{
+totalPrice+=itemsOrdered.get(i).getPrice();
+}else
+{
+minPrice=0.0;//Elimino il prezzo di una sola pizza
+}
+}else
+{
+totalPrice+=itemsOrdered.get(i).getPrice();
+}
+}
+if(totalPrice>100.0)
+{
+totalPrice*=0.95;
+}
+return totalPrice;
+}
 }
