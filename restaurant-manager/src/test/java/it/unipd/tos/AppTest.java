8763054003652e356da856eb323c69be323b95fc
apegorar <apegorar@t47.torre.math.unipd.it>
2018-12-10 13:55:58 +0100

    Aggiornati Test per TDD

--- restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
@@ -15,26 +15,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
     @Test
     public void SumOfPrice_EmptyList_Expected0Test()
     {
