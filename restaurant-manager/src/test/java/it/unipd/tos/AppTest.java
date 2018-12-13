3ad4860bda86fad4569eea6cac767c82c5d5e4ab
AlessandroPegoraro <spampego@gmail.com>
2018-12-13 10:03:20 +0100

    Aggiunti test su ValuesOf per enum ItemType

--- restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
@@ -3,7 +3,6 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
-import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import it.unipd.tos.business.exception.RestaurantBillException;
@@ -14,47 +13,75 @@ public class AppTest {
 	private App testApp=new App();
 	
     @Test
-    public void SumOfPrice_EmptyList_Expected0()//0 0
+    public void SumOfPrice_EmptyList_Expected0()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
     		double result=testApp.getOrderPrice(list);
-    		assertEquals(0.0, result,0.0001);//delta 10^-4
+    		assertEquals(0.0, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		fail(e.getMessage());
+    		System.out.println(e.getMessage());
     	}	
     }
 
 	@Test
-	public void TestOfType_ListWithPizza_TypeOfPizza()
+	public void TestOfType_WithPizza_TypeOfPizza()
 	{
 		MenuItem p1=new MenuItem(MenuItem.itemType.Pizza, "Margherita", 3.0);
 		assertEquals(MenuItem.itemType.Pizza,p1.getType());
 	}
 
         @Test
-	public void TestOfType_ListWithPrimo_TypeOfPrimo()
+	public void TestOfType_WithPrimo_TypeOfPrimo()
 	{
 		MenuItem k1=new MenuItem(MenuItem.itemType.Primo, "Pasta", 5.0);
 		assertEquals(MenuItem.itemType.Primo,k1.getType());
 	}
     
 	@Test
-	public void TestOfName_ListWithPizza_NameOfPizza()
+	public void TestOfName_WithPizza_NameOfPizza()
 	{
 		MenuItem p1=new MenuItem(MenuItem.itemType.Pizza, "Margherita", 3.0);
 		assertEquals("Margherita",p1.getName());
 	}
 
         @Test
-	public void TestOfName_ListWithPrimo_NameOfPrimo()
+	public void TestOfName_WithPrimo_NameOfPrimo()
 	{
 		MenuItem k1=new MenuItem(MenuItem.itemType.Primo, "Pasta", 5.0);
 		assertEquals("Pasta",k1.getName());
 	}
 
     @Test
-    public void SumOfPrice_MoreThan20Elements_RestaurantBillExceptionThrown()// 15 15
+    public void TestOfValues_WithPizza_TypeOfPizza()
+    {
+    	MenuItem p1=new MenuItem(MenuItem.itemType.Pizza, "Margherita", 3.0);
+    	assertEquals(MenuItem.itemType.valueOf("Pizza"),p1.getType());
+    }
+
+    @Test
+    public void TestOfValues_WithPrimo_TypeOfPrimo()
+    {
+    	MenuItem k1=new MenuItem(MenuItem.itemType.Primo, "Pasta", 5.0);
+    	assertEquals(MenuItem.itemType.valueOf("Primo"),k1.getType());
+    }
+        
+    @Test
+    public void TestOfNPrice_WithPizza_Price0()
+    {
+        MenuItem p1=new MenuItem(MenuItem.itemType.Pizza, "Margherita", -3.0);
+        assertEquals(0.0,p1.getPrice(),0.0001);
+    }
+
+    @Test
+    public void TestOfNPrice_WithPrimo_Price0()
+    {
+        MenuItem k1=new MenuItem(MenuItem.itemType.Primo, "Pasta", -5.0);
+        assertEquals(0.0,k1.getPrice(),0.0001);
+    }
+    
+    @Test
+    public void SumOfPrice_MoreThan20Elements_RestaurantBillExceptionThrown()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -68,14 +95,14 @@ public class AppTest {
     			list.add(p1); list.add(k1); list.add(p2); list.add(k2); list.add(p3); list.add(k3); 
     		}
     		testApp.getOrderPrice(list);
-    		fail("Situazione di Errore non raggiunta");
+    		System.out.println("Situazione di Errore non raggiunta");
     	}catch(RestaurantBillException e) {
     		assertEquals("Ordinazione Fallita",e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_OnlyMoreThan20Pizze_RestaurantBillExceptionThrown()// 25 0
+    public void SumOfPrice_OnlyMoreThan20Pizze_RestaurantBillExceptionThrown()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -88,14 +115,14 @@ public class AppTest {
     			list.add(p1); list.add(p2); list.add(p3); list.add(p4); list.add(p5); 
     		}
     		testApp.getOrderPrice(list);
-    		fail("Situazione di Errore non raggiunta");
+    		System.out.println("Situazione di Errore non raggiunta");
     	}catch(RestaurantBillException e) {
     		assertEquals("Ordinazione Fallita",e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_OnlyMoreThan20Primi_RestaurantBillExceptionThrown()// 0 24
+    public void SumOfPrice_OnlyMoreThan20Primi_RestaurantBillExceptionThrown()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -106,14 +133,14 @@ public class AppTest {
     			list.add(k1); list.add(k2); list.add(k3); 
     		}
     		testApp.getOrderPrice(list);
-    		fail("Situazione di Errore non raggiunta");
+    		System.out.println("Situazione di Errore non raggiunta");
     	}catch(RestaurantBillException e) {
     		assertEquals("Ordinazione Fallita",e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_MoreThan20PizzeAndMoreThan20Primi_RestaurantBillExceptionThrown()// 28 21
+    public void SumOfPrice_MoreThan20PizzeAndMoreThan20Primi_RestaurantBillExceptionThrown()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -128,14 +155,14 @@ public class AppTest {
     			list.add(p1); list.add(k1); list.add(p2); list.add(k2); list.add(p3); list.add(k3); list.add(p4);
     		}
     		testApp.getOrderPrice(list);
-    		fail("Situazione di Errore non raggiunta");
+    		System.out.println("Situazione di Errore non raggiunta");
     	}catch(RestaurantBillException e) {
     		assertEquals("Ordinazione Fallita",e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20AndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()// 18 0 >100
+    public void SumOfPrice_LessThan20AndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -146,14 +173,14 @@ public class AppTest {
     			list.add(p1); list.add(p2); list.add(p3); 
     		}
     		double result=testApp.getOrderPrice(list);
-    		assertEquals(97.85, result,0.0001);//delta 10^-4
+    		assertEquals(97.85, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		fail(e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()// 7 0 >100
+    public void SumOfPrice_LessThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -166,14 +193,14 @@ public class AppTest {
     		MenuItem p7=new MenuItem(MenuItem.itemType.Pizza, "Noci e Capperi", 17.5);
     		list.add(p1); list.add(p2); list.add(p3); list.add(p4); list.add(p5); list.add(p6);  list.add(p7);
     		double result=testApp.getOrderPrice(list);
-    		assertEquals(104.025, result,0.0001);//delta 10^-4
+    		assertEquals(104.025, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		fail(e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20ElementsAnd10OrLessPizzeTotalPriceLessThan100Euro_BillCalculated()// 6 * <100
+    public void SumOfPrice_LessThan20ElementsAnd10OrLessPizzeTotalPriceLessThan100Euro_BillCalculated()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -188,14 +215,14 @@ public class AppTest {
 			MenuItem k3=new MenuItem(MenuItem.itemType.Primo, "Lasagne", 7.5);    		
     		list.add(p1); list.add(k1); list.add(p2); list.add(k2); list.add(p3); list.add(k3); list.add(p4); list.add(p5); list.add(p6);
     		double result=testApp.getOrderPrice(list);
-    		assertEquals(69.0, result,0.0001);//delta 10^-4
+    		assertEquals(69.0, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		fail(e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20ElementsAndMoreThan10PizzeTotalPriceLessThan100Euro_BillCalculatedAndLeastExpensivePizzaGratis()// 12 * <100
+    public void SumOfPrice_LessThan20ElementsAndMoreThan10PizzeTotalPriceLessThan100Euro_BillCalculatedAndLeastExpensivePizzaGratis()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -211,14 +238,14 @@ public class AppTest {
 			MenuItem k3=new MenuItem(MenuItem.itemType.Primo, "Lasagne", 7.5);
 			list.add(k1); list.add(k2); list.add(k3);
 			double result=testApp.getOrderPrice(list);
-    		assertEquals(66.5, result,0.0001);//delta 10^-4
+    		assertEquals(66.5, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		assertEquals("Ordinazione Fallita",e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20ElementsAnd10OrLessPizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()// 6 * >100
+    public void SumOfPrice_LessThan20ElementsAnd10OrLessPizzeTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -233,14 +260,14 @@ public class AppTest {
 			MenuItem k3=new MenuItem(MenuItem.itemType.Primo, "Lasagne", 7.5);
 			list.add(k1); list.add(k2); list.add(k3);
 			double result=testApp.getOrderPrice(list);
-    		assertEquals(42.0, result,0.0001);//delta 10^-4
+    		assertEquals(42.0, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		assertEquals("Ordinazione Fallita",e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20ElementsAndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()// 12 * >100
+    public void SumOfPrice_LessThan20ElementsAndMoreThan10PizzeTotalPriceMoreThan100Euro_BillDiscountedOf5PercentAndLeastExpensivePizzaGratis()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -257,14 +284,14 @@ public class AppTest {
 			MenuItem k4=new MenuItem(MenuItem.itemType.Primo, "Pasta con Gamberetti", 16.0);
 			list.add(k1); list.add(k2); list.add(k3); list.add(k4);
 			double result=testApp.getOrderPrice(list);
-    		assertEquals(106.875, result,0.0001);//delta 10^-4
+    		assertEquals(106.875, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		assertEquals("Ordinazione Fallita",e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20PrimiTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()// 0 * >100
+    public void SumOfPrice_LessThan20PrimiTotalPriceMoreThan100Euro_BillDiscountedOf5Percent()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -276,14 +303,14 @@ public class AppTest {
     			list.add(k1); list.add(k2); list.add(k3); list.add(k4);
     		}			
 			double result=testApp.getOrderPrice(list);
-    		assertEquals(179.55, result,0.0001);//delta 10^-4
+    		assertEquals(179.55, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		assertEquals("Ordinazione Fallita",e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 
     @Test
-    public void SumOfPrice_LessThan20PrimiTotalPriceLessThan100Euro_BillDiscountedOf5Percent()// 0 * <100
+    public void SumOfPrice_LessThan20PrimiTotalPriceLessThan100Euro_BillDiscountedOf5Percent()
     {
     	try{
     		ArrayList<MenuItem> list = new ArrayList<MenuItem>();
@@ -293,9 +320,9 @@ public class AppTest {
     		MenuItem k4=new MenuItem(MenuItem.itemType.Primo, "Pasta con Gamberetti", 16.0);
     		list.add(k1); list.add(k2); list.add(k3); list.add(k4);		
 			double result=testApp.getOrderPrice(list);
-    		assertEquals(63, result,0.0001);//delta 10^-4
+    		assertEquals(63, result,0.0001);
     	}catch(RestaurantBillException e) {
-    		assertEquals("Ordinazione Fallita",e.getMessage());
+    		System.out.println(e.getMessage());
     	}
     }
 }
